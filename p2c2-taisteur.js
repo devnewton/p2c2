02801430f1ca1e981a8ddc79c4b3325b33eebd0d
devnewton <devnewton@bci.im>
2016-06-19 21:15:12 +0200

    add default sample to taisteur

--- p2c2-taisteur.js
+++ p2c2-taisteur.js
@@ -1,11 +1,18 @@
 var convertButton = document.getElementById('convert');
-
-convertButton.addEventListener('click', function() {
-	var inputArea = document.getElementById('input');
-	var outputArea = document.getElementById('output');
-try {
-	outputArea.value= p2c2.parse(inputArea.value);
-} catch(err) {
-outputArea.value = err;
+var inputArea = document.getElementById('input');
+var outputArea = document.getElementById('output');
+function convert() {
+	try {
+		outputArea.value= p2c2.parse(inputArea.value);
+	} catch(err) {
+		outputArea.value = err;
+	}
 }
-});
+convertButton.addEventListener('click', convert);
+
+function init() {
+	inputArea.value = '10:11:03 [:bondour] les <b>moules<</b>';
+	convert();
+}
+document.readyState === "interactive" || document.readyState === "complete" ? init() : document.addEventListener("DOMContentLoaded", init);
+
